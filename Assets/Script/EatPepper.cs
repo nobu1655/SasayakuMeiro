6a6d1d69ef425d12c7617113dc3e18dfe965766a
Jec nobu <24ci0107@jec.ac.jp>
2025-12-09 15:42:18 +0900

    ミニマップをメインシーンに実装した

--- Assets/Script/EatPepper.cs
+++ Assets/Script/EatPepper.cs
@@ -5,12 +5,19 @@ using UnityEngine;
 public class EatPepper : MonoBehaviour
 {
     public int scoreValue = 1;
+    public GameObject mapObject;
 
     void OnTriggerEnter(Collider Other)
     {
         if(Other.CompareTag("Player"))
         {
+            //if (GemEventManager.Instance != null)
+            //{
+            //    GemEventManager.Instance.CollectGem(uniqueGemID);
+            //}
+
             ScoreManager.Instance.EatOneDot();
+            Destroy(mapObject);
             Destroy(gameObject);
         }
     }
