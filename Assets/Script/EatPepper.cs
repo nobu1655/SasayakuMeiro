0b3ad9fec5c462570b6555cddca8dc29e50b8a4c
Jec nobu <24ci0107@jec.ac.jp>
2025-12-16 16:08:59 +0900

    フォントの追加、オーディオの追加

--- Assets/Script/EatPepper.cs
+++ Assets/Script/EatPepper.cs
@@ -1,22 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EatPepper : MonoBehaviour
 {
-    public int scoreValue = 1;
+    public int scoreToDecrease = 1;
     public GameObject mapObject;
+    private ScoreManager scoreManager;
 
     void OnTriggerEnter(Collider Other)
     {
         if(Other.CompareTag("Player"))
         {
-            //if (GemEventManager.Instance != null)
-            //{
-            //    GemEventManager.Instance.CollectGem(uniqueGemID);
-            //}
-
-            ScoreManager.Instance.EatOneDot();
+            if (scoreManager != null)
+            {
+                scoreManager.DecreaseScore(scoreToDecrease);
+            }
             Destroy(mapObject);
             Destroy(gameObject);
         }
--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -1,24 +1,35 @@
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using TMPro; // TextMeshProを使用するために必要
 
 public class ScoreManager : MonoBehaviour
 {
-    public static ScoreManager Instance;
-    public TMP_Text ScoreText;
-    public int totalDots = 105;
-    public int remainingDots;
+    // インスペクターからTextMeshProのコンポーネントをD&Dで設定
+    public TextMeshProUGUI scoreNumberText;
 
-    void Awake()
+    // 現在のスコア（宝石の残り数など）
+    private int currentScore = 208;
+
+    void Start()
     {
-        Instance = this;
-        remainingDots = totalDots;
+        // 初期表示を更新
+        UpdateScoreDisplay();
     }
 
-  public void EatOneDot()
+    // 外部からスコアを減らすためのパブリックメソッド
+    public void DecreaseScore(int amount)
     {
-        remainingDots--;
-        // Debug.Log("残りの個数" + remainingDots + "/" + totalDots);
-        ScoreText.text = remainingDots + "/" + totalDots;
+        currentScore -= amount;
+
+        // 表示を更新
+        UpdateScoreDisplay();
+    }
+
+    // TextMeshProの表示を更新するメソッド
+    private void UpdateScoreDisplay()
+    {
+        if (scoreNumberText != null)
+        {
+            scoreNumberText.text = currentScore.ToString();
+        }
     }
 }
