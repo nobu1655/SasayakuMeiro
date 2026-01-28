5b8483618292bef40a957a1759902e27d7d03746
Renjingchuan <24ci0323@jec.ac.jp>
2026-01-28 11:55:43 +0900

    Rankを追加しました

--- Assets/GolemiteGames/TimersMadeEasyLite/Scripts/Timer.cs
+++ Assets/GolemiteGames/TimersMadeEasyLite/Scripts/Timer.cs
@@ -63,10 +63,13 @@ public class Timer : MonoBehaviour
     bool timerRunning = false;
     bool timerPaused = false;
     public double timeRemaining;
-    
+    public static Timer Instance;
+
+
 
     private void Awake()
     {
+        Instance = this;
 
         if(!standardText)
         if(GetComponent<Text>())
---
+++ Assets/Script/Score.cs
@@ -0,0 +1,10 @@
+public static class Score
+{
+    public static string CalculateRank(float timeLeft)
+    {
+        if (timeLeft >= 50f) return "S"; 
+        if (timeLeft >= 40f) return "A"; 
+        if (timeLeft >= 20f) return "B"; 
+        return "C";
+    }
+}
